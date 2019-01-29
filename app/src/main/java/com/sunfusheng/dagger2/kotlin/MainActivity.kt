5321a9639088fad31e8a11acdba77d9279e0985b
sunfusheng <sfsheng0322@gmail.com>
2019-01-30 00:37:27 +0800

    add section1

--- app/src/main/java/com/sunfusheng/dagger2/kotlin/MainActivity.kt
+++ app/src/main/java/com/sunfusheng/dagger2/kotlin/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.sunfusheng.dagger2.kotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = User("sunfusheng")
+
+        val provider:IOwnerProvider = Repo("GitHub")
+        provider.setOwner(user)
+
+        val repo = Repo("GitHub")
+        repo.setOwner(user)
+
+        Log.d("--->", user.toString())
+        Log.d("--->", repo.toString())
     }
 }
+
+
+data class User constructor(val userName: String)
+
+interface IOwnerProvider {
+    fun setOwner(owner: User)
+}
+
+data class Repo constructor(val repoName: String) : IOwnerProvider {
+    lateinit var user: User
+
+    override fun setOwner(owner: User) {
+        this.user = owner
+    }
+}
+
