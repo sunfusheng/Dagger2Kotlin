1aca98ff06163f901feb733c2015317ea50cbe69
sunfusheng <sfsheng0322@gmail.com>
2019-01-31 21:57:01 +0800

    add section2

--- app/src/main/java/com/sunfusheng/dagger2/kotlin/section2/IMsgView.kt
+++ app/src/main/java/com/sunfusheng/dagger2/kotlin/section2/IMsgView.kt
@@ -3,7 +3,6 @@ package com.sunfusheng.dagger2.kotlin.section2
 /**
  * @author sunfusheng on 2019/1/31.
  */
-interface IPresenter {
-    fun init()
-    fun unInit()
+interface IMsgView{
+    fun showMsg(msg: String)
 }
