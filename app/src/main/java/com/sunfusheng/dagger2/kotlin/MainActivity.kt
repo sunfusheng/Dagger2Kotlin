ca8db54204dd68c22c6faa981d71cbb85df00ba8
sunfusheng <sfsheng0322@gmail.com>
2019-02-11 22:18:46 +0800

    删除多余代码

--- app/src/main/java/com/sunfusheng/dagger2/kotlin/MainActivity.kt
+++ app/src/main/java/com/sunfusheng/dagger2/kotlin/MainActivity.kt
@@ -2,11 +2,9 @@ package com.sunfusheng.dagger2.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class MainActivity : AppCompatActivity() {
 
-    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/sunfusheng/dagger2/kotlin/mvp_with_dagger2/RepoFragment.kt
+++ app/src/main/java/com/sunfusheng/dagger2/kotlin/mvp_with_dagger2/RepoFragment.kt
@@ -33,11 +33,6 @@ class RepoFragment : Fragment(), IRepoView {
         mPresenter.fetchRepo()
     }
 
-    override fun onDestroyView() {
-        lifecycle.removeObserver(mPresenter)
-        super.onDestroyView()
-    }
-
     override fun onRepoCallback(repo: Repo) {
         vText.text = repo.reponame
     }
